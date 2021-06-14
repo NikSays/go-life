97a5ded2f8253f342c20ad8e4029753c5955ca35
niksays <ns@lyceum.ort.md>
2021-06-14 20:34:36 +0300

    first

---
+++ fieldUtils.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"image"
+	"log"
+	"os"
+	"time"
+
+	ui "github.com/gizak/termui/v3"
+)
+
+func (field *FieldT) iterateField(action func(field *FieldT, x, y int)) {
+	for y := 0; y < SIZE; y++ {
+		for x := 0; x < SIZE; x++ {
+			action(field, x, y)
+		}
+	}
+}
+func (field *FieldT) read(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal("Couldn't open file")
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	y := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) > SIZE {
+			log.Print("Too wide, turncating")
+			time.Sleep(time.Second)
+		}
+		for x := 0; (x < SIZE) && (x < len(line)); x++ {
+			if line[x] == CELLCHAR {
+				field[y][x] = true
+			}
+		}
+		y++
+		if y > SIZE {
+			log.Print("Too high, turncating")
+			time.Sleep(time.Second)
+			break
+		}
+	}
+}
+func (field *FieldT) createMap(c *ui.Canvas) {
+	printOne := func(field *FieldT, x, y int) {
+		if field[y][x] {
+			c.SetPoint(image.Pt(x, y), ui.ColorWhite)
+		} // else {
+		// log.Print(x, y)
+		//c.SetPoint(image.Pt(11, 11), ui.ColorRed)
+		//}
+	}
+	field.iterateField(printOne)
+}
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"image"
+	"log"
+	"time"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/drawille"
+)
+
+const SIZE = 212
+const CELLCHAR = 'O'
+const FPS = 3000
+
+var temp image.Rectangle
+var CLEARBUF = ui.NewBuffer(temp)
+
+type FieldT [SIZE][SIZE]bool
+
+func main() {
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+
+	var field FieldT
+	field.read("field.in")
+
+	window := ui.NewCanvas()
+	window.SetRect(0, 0, SIZE, SIZE)
+
+	uiEvents := ui.PollEvents()
+	ticker := time.NewTicker(time.Second / FPS).C
+	i := 0
+	for {
+		i++
+		select {
+		case e := <-uiEvents:
+			switch e.ID {
+			case "q", "<C-c>":
+				return
+			}
+		case <-ticker:
+			window.Canvas.CellMap = make(map[image.Point]drawille.Cell)
+			field.createMap(window)
+			field.conwayStep()
+			ui.Render(window)
+		}
+	}
+}
+
+func (field *FieldT) displacedDiagonal(displacement int) {
+	updateCell := func(field *FieldT, x int, y int) {
+		if x+displacement == y {
+			field[y][x] = true
+		} else {
+			field[y][x] = false
+		}
+	}
+	field.iterateField(updateCell)
+}
+func (field *FieldT) conwayStep() {
+	var newField FieldT
+	//Create torus coordinates
+	//Example: for size:=20, coordinates are from 0 to 19
+	//         warpCoordinates turns -1 to 19, 20 to 0
+	wrapCoordinates := func(x, y int) (int, int) {
+		warpx := (x + SIZE) % SIZE
+		warpy := (y + SIZE) % SIZE
+		return warpx, warpy
+	}
+	//Count all live cells around given x,y; excluding x,y cell itself
+	//                                       including those around screen corners
+	countNeighbors := func(field *FieldT, x, y int) (sum int) {
+		for j := y - 1; j <= y+1; j++ {
+			for i := x - 1; i <= x+1; i++ {
+				if (i == x) && (j == y) {
+					continue
+				}
+				warpi, warpj := wrapCoordinates(i, j)
+				if field[warpj][warpi] {
+					sum++
+				}
+			}
+		}
+		return
+	}
+	//
+	newField = *field
+	updateCell := func(field *FieldT, x, y int) {
+		switch count := countNeighbors(field, x, y); {
+		case count < 2:
+			newField[y][x] = false
+		case count == 3:
+			newField[y][x] = true
+		case count > 3:
+			newField[y][x] = false
+		}
+	}
+	field.iterateField(updateCell)
+	*field = newField
+}
